32b074290fce72192e3cb7a79b339c1983ef972f
fabianlee1211 <chleefabian@gmail.com>
2019-09-01 17:08:42 +0800

    refactor: social buttons hover style

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 const Icon = ({ icon, link, ...props }) => {
   const src = require(`./icons/icon-${icon}.svg`)
+  const iconStyle = {
+    mask: `url(${src})`,
+    maskSize: 'cover',
+  }
   if (link) {
     return (
       <a href={link} target="_blank" rel="nofollow noopener noreferrer">
-        <img src={src} {...props} alt={`icon-${icon}`} />
+        <span style={iconStyle} {...props} />
       </a>
     )
   }
-  return <img src={src} {...props} alt={`icon-${icon}`} />
+  return <span style={iconStyle} {...props} />
 }
 
 export default Icon
