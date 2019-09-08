17b87aa26eef472bf5ea43b5a71efd9167767f8d
Fabian Lee <chleefabian@gmail.com>
2019-09-08 16:13:02 +0800

    fix: add webkit prefix to icon mask

--- src/components/templates/GetInTouchSection/index.js
+++ src/components/templates/GetInTouchSection/index.js
@@ -1,8 +1,7 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../../atoms/Layout'
 import Icon from '../../atoms/Icon'
-import { ThemeContext } from '../../../context'
 import './styles.scss'
 
 const GetInTouchSection = () => {
@@ -19,18 +18,29 @@ const GetInTouchSection = () => {
       }
     }
   `)
-  const themeContext = useContext(ThemeContext)
-  const { theme } = themeContext
   const { email, socialMedia } = data.site.siteMetadata
   return (
-    <Layout className="GetInTouchSection" innerClassName="GetInTouchSection__Wrapper">
+    <Layout
+      className="GetInTouchSection"
+      innerClassName="GetInTouchSection__Wrapper"
+    >
       <h2 className="GetInTouchSection__Title">Get In Touch</h2>
       <p className="GetInTouchSection__Text">
-        Shoot me an email at <span className="GetInTouchSection__Email">{email}</span> if you have anything in mind.
-        <br />You can also visit me on other platforms:
+        Shoot me an email at{' '}
+        <span className="GetInTouchSection__Email">{email}</span> if you have
+        anything in mind.
+        <br />
+        You can also visit me on other platforms:
       </p>
       <div className="GetInTouchSection__SocialMediaButtons">
-        {socialMedia.map(media => <Icon key={media.platform} className="GetInTouchSection__SocialMediaButton" icon={media.platform} link={media.link} />)}
+        {socialMedia.map(media => (
+          <Icon
+            key={media.platform}
+            className="GetInTouchSection__SocialMediaButton"
+            icon={media.platform}
+            link={media.link}
+          />
+        ))}
       </div>
     </Layout>
   )
